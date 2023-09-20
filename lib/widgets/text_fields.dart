a0daedf4bd7679de6cab50b0de3afd05bdbf5995
asumah banda <asumahbanda3@gmail.com>
2023-09-20 11:03:43 +0000

    made password obscure

--- lib/widgets/text_fields.dart
+++ lib/widgets/text_fields.dart
@@ -5,13 +5,15 @@ class MyTextField extends StatelessWidget {
     super.key,
     required this.name,
     required this.label,
-    this.trailing, required this.controller,
+    this.trailing,
+    this.obsureText = false,
+    required this.controller,
   });
   final String name;
   final String label;
-  final Icon? trailing;
+  final Widget? trailing;
   final TextEditingController controller;
-
+  final bool obsureText;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -26,9 +28,11 @@ class MyTextField extends StatelessWidget {
               ?.copyWith(fontWeight: FontWeight.bold),
         ),
         SizedBox(height: 7),
-        TextField(controller:  controller,
+        TextField(
+          controller: controller,
+          obscureText: obsureText,
           decoration: InputDecoration(
-            contentPadding: EdgeInsets.symmetric(horizontal: 18),
+            contentPadding: const EdgeInsets.symmetric(horizontal: 18),
             suffixIcon: trailing,
             focusedBorder: border,
             labelText: label,
